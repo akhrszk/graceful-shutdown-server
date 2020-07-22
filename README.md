aaf482d151fffd7d564c0bb65c6574384d8a406b
Akihiro Suzuki <28677705+akhrszk@users.noreply.github.com>
2020-07-23 04:30:52 +0900

    Shutdown処理にタイムアウトを設定(5秒)

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# 接続中のConnectionが全て閉じるのを待ってからHttpServerをShutdownする
+# graceful-shutdown-server
+
+## 概要
+
+接続中のConnectionが全て閉じるのを待ってからHttpServerをShutdownする
 
 **Graceful Shutdown** については以下を参照
 
 https://golang.org/pkg/net/http/#Server.Shutdown
 
-今回の実装では、 `interrupt signal` を受け取ったら `http.Server` を `Shutdown` する
+`interrupt signal` を受け取ったら `http.Server` を `Shutdown` する
+
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
   "os/signal"
   "fmt"
   "log"
+  "time"
   "net/http"
 )
 
@@ -23,9 +24,12 @@ func main() {
     log.Print("HttpServer Up Success!")
 
     <-quit
-    ctx := context.Background()
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
     log.Print("Shutting down...")
-    httpServer.Shutdown(ctx)
+    if err := httpServer.Shutdown(ctx); err != nil {
+      log.Fatal("Server forced to shutdown:", err)
+    }
   }()
 
   http.HandleFunc("/", handler)
